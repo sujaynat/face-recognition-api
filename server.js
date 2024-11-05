fdb6a0bc511efb5a8e994ca3b63a9e8f6d5ecda2
Sujay Natarajan <sujaynat@Sujays-MBP.attlocal.net>
2024-11-05 17:50:55 -0600

    updating render postgres details

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const signin = require('./controllers/signin');
 const db = require('knex')({
     client: 'pg',
     connection: {
-      host: '127.0.0.1',
-      port: 5432,
-      user: 'sujaynat',
-      password: '',
-      database: 'smart-brain',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
   });
 
