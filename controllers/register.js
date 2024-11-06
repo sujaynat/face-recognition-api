5d822e267cd9e46b98444ea7fff0cd4783ebdea0
Sujay Natarajan <sujaynat@Sujays-MBP.attlocal.net>
2024-11-05 22:27:17 -0600

    adding env variables to connect to remote postgres

--- controllers/register.js
+++ controllers/register.js
@@ -28,7 +28,7 @@ const handleRegister = (req, res, db, bcrypt) => {
         .catch(trx.rollback);
     })
     
-    .catch(err=> res.status(400).json("Unable to Register"));
+    .catch(err=>console.log(err));
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -5,72 +5,38 @@ const app = express();
 const bcrypt = require('bcrypt-nodejs');
 const { console } = require('inspector');
 const signin = require('./controllers/signin');
-
-const db = require('knex')({
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-  });
+const register = require('./controllers/register');
+const profile = require('./controllers/profile');
 
 app.use(cors());
 app.use(express.json()); 
 
 const PORT = process.env.PORT;
+
+const db = require('knex')({
+    client: 'pg',
+    connection: {
+      connectionString:process.env.DB_URL,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+  });
+
+
 app.get('/', (req,res) =>{
     res.json(db.users);
 })
 
+
 app.post('/signin', signin.handleSignin(db, bcrypt))
 
-app.post('/register', (req,res) => {
-    const {email,name,password} = req.body;
-    
-    
-    // Load hash from your password DB.
-    var hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash:hash,
-            email:email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail =>{
-            trx('users')
-            .returning('*')
-            .insert({
-                email: loginEmail[0].email,
-                name: name,
-                joined: new Date()
-            }).then(user =>{
-                res.json(user[0])
-            })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    })
-    
-    .catch(err=> res.status(400).json("Unable to Register"));
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.get('/profile/:id',(req,res) =>{
-    const {id} = req.params;
-    db.select('*').from('users')
-    .where({id})
-    .then(user=>{
-        if(user.length){
-            res.json(user[0]);
-        }else{
-            res.status(400).json('Not Found');
-        }
-    })
-    .catch(err => res.status(400).json('Error Getting User'));
-})
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post("/imageurl", async function(req, res){  
     var data = await image.handleApiCall(req);
